861e2fa4d686daf822ca81fde936bc47549c81e6
borishdez <borisnandez93@gmail.com>
2024-06-04 17:21:24 -0600

    05 creado

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { setupCounter } from './counter.ts'
 //import './topics/01-basic-types.ts';
 //import './topics/02-object-interface';
 //import './topics/03-functions';
-import './topics/04-homework-types';
+//import './topics/04-homework-types';
+import './topics/05-basic-destructuring';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
