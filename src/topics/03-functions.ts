e2c41a8c518f15dd8f81b3b0b2b0ca74bbe37b91
borishdez <borisnandez93@gmail.com>
2024-06-04 16:24:09 -0600

    function arrow tipo string, regresa result en string

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -4,8 +4,13 @@ function addNumbers(a:number, b:number){
     return a + b;
 }
 
-const result:number = addNumbers(1, 2);
+const addNumbersArrow = (a:number, b:number):string => {
+    return `${ a + b }`;
+}
 
-console.log({result});
+const result:number = addNumbers(1, 2);
+const result2:string = addNumbersArrow(1, 2);
+
+console.log({result, result2});
 
 export {};
