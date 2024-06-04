8c1d4ad5e2898bf9a156b8082e40db4867fcafde
borishdez <borisnandez93@gmail.com>
2024-06-04 17:04:11 -0600

    04 creado

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 //import './topics/01-basic-types.ts';
 //import './topics/02-object-interface';
-import './topics/03-functions';
+//import './topics/03-functions';
+import './topics/04-homework-types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
