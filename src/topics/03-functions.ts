cba5f294d18dc0ac668cde90310a63f3318a49bb
borishdez <borisnandez93@gmail.com>
2024-06-04 16:51:44 -0600

    healcharacter con parametros

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -12,10 +12,21 @@ function multiply( firstNumber:number, secondNumber?:number, base:number = 2){
     return firstNumber * base;
 }
 
-const result:number = addNumbers(1, 2);
+/*const result:number = addNumbers(1, 2);
 const result2:string = addNumbersArrow(1, 2);
 const multiplyResult:number = multiply(5);
+console.log({result, result2, multiplyResult});*/
 
-console.log({result, result2, multiplyResult});
+interface Character {
+    name: string,
+    hp: number,
+    showHp: () => void
+}
+
+const healCharacter = ( character, amount:number ) => {
+
+    character.pv += amount;
+
+}
 
 export {};
