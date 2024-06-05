ee33e110a437591a3f05aec91374e378a7190c11
borishdez <borisnandez93@gmail.com>
2024-06-05 16:09:29 -0600

    extender clase

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -7,6 +7,16 @@ export class Person {
     constructor( public name: string, private address: string = 'No Adress' ) {}
 }
 
-const ironman = new Person('Iron-Man');
+export class Hero extends Person {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string
+    ){
+        super( realName, 'New York' )
+    }
+}
+
+const ironman = new Hero('Iron-Man', 45, 'Tony Stark');
 
 console.log(ironman);
