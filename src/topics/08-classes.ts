dab30f7a4123e75e89180f107e05d48464144d95
borishdez <borisnandez93@gmail.com>
2024-06-05 15:42:38 -0600

    class Person w constructor

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,5 +1,11 @@
 
 
 export class Person {
+    public name: string;
+    public address: string;
 
+    constructor() {
+        this.name = 'Boris';
+        this.address = 'Santa Tecla';
+    }
 }
