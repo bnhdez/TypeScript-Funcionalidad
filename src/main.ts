28a950fc66efc61e372e16791497fdca70e48fcc
borishdez <borisnandez93@gmail.com>
2024-06-05 15:16:03 -0600

    07 agregado

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import { setupCounter } from './counter.ts'
 //import './topics/03-functions';
 //import './topics/04-homework-types';
 //import './topics/05-basic-destructuring';
-import './topics/06-function-destructuring';
+//import './topics/06-function-destructuring';
+import './topics/07-import-export.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
