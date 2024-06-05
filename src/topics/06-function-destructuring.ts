777d693a3e721eb91353af83d92afc43fbe00213
borishdez <borisnandez93@gmail.com>
2024-06-05 14:43:18 -0600

    destructuracion completa en ejercicio

--- src/topics/06-function-destructuring.ts
+++ src/topics/06-function-destructuring.ts
@@ -20,14 +20,16 @@ interface TaxCalculationOptions {
     products: Product[];
 }
 
-function taxCalculation( options:TaxCalculationOptions ): number[] {
+function taxCalculation( options:TaxCalculationOptions ): [number, number] {
+    const {tax, products} = options;
+
     let total = 0;
 
-    options.products.forEach( product => {
-        total += product.price;
+    products.forEach( ({ price }) => {
+        total += price;
     } )
 
-    return[total, total * options.tax];
+    return[total, total * tax];
 }
 
 
@@ -36,13 +38,13 @@ const shoppingCart = [phone, tablet];
 const tax = 0.15;
 
 
-const result = taxCalculation({
+const [total, totalTax] = taxCalculation({
     products: shoppingCart,
     tax: tax
 })
 
-console.log('Total: ', result[0]);
-console.log('Tax: ', result[1]);
+console.log('Total: ', total);
+console.log('Tax: ', totalTax);
 
 
 
