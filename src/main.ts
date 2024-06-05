38654b34058969750676f530c1b3c3765491fc13
borishdez <borisnandez93@gmail.com>
2024-06-05 15:46:26 -0600

    public y private properties

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import { setupCounter } from './counter.ts'
 //import './topics/04-homework-types';
 //import './topics/05-basic-destructuring';
 //import './topics/06-function-destructuring';
-import './topics/07-import-export.ts';
+//import './topics/07-import-export.ts';
+import './topics/08-classes.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -2,10 +2,14 @@
 
 export class Person {
     public name: string;
-    public address: string;
+    private address: string;
 
     constructor() {
         this.name = 'Boris';
         this.address = 'Santa Tecla';
     }
 }
+
+const ironman = new Person();
+
+console.log(ironman.address);
