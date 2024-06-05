9243f2204ee4f2ec74b2e4f6b057454a98b41606
borishdez <borisnandez93@gmail.com>
2024-06-05 16:21:35 -0600

    composicion sobre herencia

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -4,10 +4,14 @@ export class Person {
     //public name: string;
     //private address: string;
 
-    constructor( public name: string, private address: string = 'No Adress' ) {}
+    constructor( 
+        public firstName: string,
+        public lastName: string,
+        private address: string = 'No Adress' 
+    ) {}
 }
 
-export class Hero extends Person {
+/*export class Hero extends Person {
     constructor(
         public alterEgo: string,
         public age: number,
@@ -15,8 +19,23 @@ export class Hero extends Person {
     ){
         super( realName, 'New York' )
     }
+}*/
+
+export class Hero {
+
+    //public person: Person;
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+    ) {
+        //this.person = new Person(realName);
+    }
 }
 
-const ironman = new Hero('Iron-Man', 45, 'Tony Stark');
+const tony  = new Person('Tony', 'Stark' ,'New York');
+const ironman = new Hero('Iron-Man', 45, 'Tony Stark', tony);
 
 console.log(ironman);
