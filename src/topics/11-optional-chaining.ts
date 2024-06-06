31bdc85f773f3b30f01d4912cf3d05cf178ba7e4
borishdez <borisnandez93@gmail.com>
2024-06-06 11:08:15 -0600

    desencadenamiento opcional

--- src/topics/11-optional-chaining.ts
+++ src/topics/11-optional-chaining.ts
@@ -2,5 +2,23 @@
 
 export interface Passanger {
     name: string,
-    children?: string[]
+    children?: string[],
 }
+
+const passenger1: Passanger = {
+    name: 'Boris',
+}
+
+const passenger2: Passanger = {
+    name: 'Melissa',
+    children: ['Natalia', 'Elizabeth']
+}
+
+const printChildren = ( passenger: Passanger ) => {
+
+    const howManyChildren = passenger.children?.length;
+
+    console.log(passenger.name, howManyChildren);
+}
+
+printChildren( passenger2 );
