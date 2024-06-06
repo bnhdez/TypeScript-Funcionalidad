f8e443076f569d8bdb2a2f15d9d60ab6f02def9f
borishdez <borisnandez93@gmail.com>
2024-06-06 10:52:35 -0600

    Decorator

--- src/topics/10-decorators.ts
+++ src/topics/10-decorators.ts
@@ -1,6 +1,14 @@
+function classDecorator(
+    constructor: any
+) {
+    return class extends constructor{
+        newProperty = 'New Property';
+        hello = 'override';
+    }
+}
 
 
-
+@classDecorator
 export class SuperClass {
 
     public myProperty: string = 'Abc123';
