934e61c5c344a8e42d441590d3c96da9371e2c50
borishdez <borisnandez93@gmail.com>
2024-06-06 10:40:08 -0600

    instancia myclass

---
+++ src/topics/10-decorators.ts
@@ -0,0 +1,17 @@
+
+
+
+export class SuperClass {
+
+    public myProperty: string = 'Abc123';
+
+    print() {
+        console.log('Hola Mundo!');
+    }
+}
+
+console.log( SuperClass );
+
+const myClass = new SuperClass;
+
+console.log( myClass );
